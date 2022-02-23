f9ac29d95d3712bc3753da83026e1ee4440da9a3
bryan cardenas <bryan.cardenas@udea.edu.co>
2022-02-23 08:57:20 -0500

    se modifica procfile

--- src/main/java/co/com/bancolombia/apiestudiantes/controllers/EstudianteController.java
+++ src/main/java/co/com/bancolombia/apiestudiantes/controllers/EstudianteController.java
@@ -5,6 +5,7 @@ package co.com.bancolombia.apiestudiantes.controllers;
 import co.com.bancolombia.apiestudiantes.DTO.EstudianteDTO;
 import co.com.bancolombia.apiestudiantes.services.EstudianteService;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,6 +30,7 @@ public class EstudianteController {
         URI uri = new URI("/estudiante");
         estudianteService.crearEstudiante(estudianteDTO);
         return ResponseEntity.created(uri).body(estudianteDTO);
+        Logger
         //return ResponseEntity.status(HttpStatus.CREATED).body("Creado corretamente");
     }
 }
